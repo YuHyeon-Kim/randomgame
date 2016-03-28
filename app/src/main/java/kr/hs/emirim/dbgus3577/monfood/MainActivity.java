1b11ddaf782e6cce13d1bb2dc2c736081b2eec81
YuHyeon-Kim <dbgus3577@gmail.com>
2016-03-28 19:15:32 +0900

    #1 Create Project, change app name

---
+++ app/src/main/java/kr/hs/emirim/dbgus3577/monfood/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.dbgus3577.monfood;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
